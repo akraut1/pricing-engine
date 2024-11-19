368c7aa27fbe95903b669bad0796bba175b48061
Aaron Kraut <129128437+akraut1@users.noreply.github.com>
2024-11-19 02:12:48 -0500

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,1 +1,36 @@
 let darkMode = localStorage.getItem('darkMode'):
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+console.log(darkMode);
+
+// check if dark mode is enabled
+// if dark mode is enabled, turn it off. If dark mode is dusabled, tturn it on
+
+const enableDarkMode = () => {
+  //1. add the class darkmode to the body
+  document.body.classList.add('darkmode');
+  // 2. update darkMode in the localStorage
+  localStorage.setItem('darkMode', 'enabled')
+};
+
+const disableDarkMode = () => {
+  //1. add the class darkmode to the body
+  document.body.classList.remove('darkmode');
+  // 2. update darkMode in the localStorage
+  localStorage.setItem('darkMode', 'null')
+};
+
+if (darkMode === 'enabled') {
+  enableDarkMOde();
+}
+
+darkModeToggle.addEventListener('click, () => {
+  darkMode = localStorage.getItem('darkMode')
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+    consle.log(darkMode);
+  } else {
+    disableDarkMode();
+    consle.log(darkMode);
+  }
+});
